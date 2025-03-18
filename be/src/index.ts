73fb22ef220ae8c92fcb5199cf2429e1fdbfd8cc
Tanmay Borse <tanmayborse28@gmail.com>
2025-03-18 23:03:27 +0530

    changed the backend url

--- be/src/index.ts
+++ be/src/index.ts
@@ -62,5 +62,9 @@ app.post("/chat", async (req, res) => {
     });
 })
 
-app.listen(3000);
+// app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
