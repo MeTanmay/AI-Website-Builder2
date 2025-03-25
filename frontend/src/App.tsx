6f50cf68d28330ef30f40dbc8053b194416c2abf
Prajwal Mandlik <prajwalmandlik2004@gmail.com>
2025-03-25 20:18:49 +0530

    new small update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
-import { Hero } from './pages/Hero';
-import { Signup } from './pages/Signup';
-import { Signin } from './pages/Signin';
+// import { Hero } from './pages/Hero';
+// import { Signup } from './pages/Signup';
+// import { Signin } from './pages/Signin';
 import { Builder } from './pages/Builder';
 import { parseXml } from './steps';
 
@@ -24,10 +24,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/signup" element={<Signup isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />} />
-        <Route path="/signin" element={<Signin isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />} />
-        <Route path="/" element={<Hero isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />} />
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/signup" element={<Signup isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />} /> */}
+        {/* <Route path="/signin" element={<Signin isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />} /> */}
+        {/* <Route path="/" element={<Hero isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />} /> */}
         <Route path="/builder" element={<Builder />} />
       </Routes>
     </BrowserRouter>
